b90bd062612b173a31dda7794ddbf142bc76a596
Jakub-Bednarski <jakub.piotr.bednarski@gmail.com>
2021-02-10 15:36:57 +0100

    https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/csharp

---
+++ Codewars/Codewars.tests/7kyu/MumblingTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Codewars._7kyu;
+
+namespace Codewars.tests._7kyu
+{
+    [TestFixture]
+    public static class AccumulTests
+    {
+
+        private static void testing(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            testing(Mumbling.Accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+            testing(Mumbling.Accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+            testing(Mumbling.Accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+            testing(Mumbling.Accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+            testing(Mumbling.Accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+        }
+    }
+}
---
+++ Codewars/Codewars/7kyu/Mumbling.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Codewars._7kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/csharp
+    /// </summary>
+    public class Mumbling
+    {
+        public static String Accum(string s)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                result.Append(s[i].ToString().ToUpper() + new string(s[i], i).ToLower());
+                if (i < s.Length - 1) result.Append("-");
+            }
+
+            return result.ToString();
+        }
+    }
+}
