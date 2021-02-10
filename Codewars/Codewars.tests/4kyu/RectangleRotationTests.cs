a21eda987269d13aed5e98bab759a3bc823529f9
Jakub-Bednarski <jakub.piotr.bednarski@gmail.com>
2021-02-10 23:46:58 +0100

    https://www.codewars.com/kata/5886e082a836a691340000c3/csharp

---
+++ Codewars/Codewars.tests/4kyu/RectangleRotationTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using Codewars._4kyu;
+
+namespace Codewars.tests._4kyu
+{
+    [TestFixture]
+    class RectangleRotationTests
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new RectangleRotation();
+
+            Assert.AreEqual(23, kata.RectangleRotationCount(6, 4));
+
+            Assert.AreEqual(65, kata.RectangleRotationCount(30, 2));
+
+            Assert.AreEqual(49, kata.RectangleRotationCount(8, 6));
+
+            Assert.AreEqual(333, kata.RectangleRotationCount(16, 20));
+        }
+    }
+}
---
+++ Codewars/Codewars/4kyu/RectangleRotation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Codewars._4kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/5886e082a836a691340000c3/csharp
+    /// </summary>
+    public class RectangleRotation
+    {
+        public int RectangleRotationCount(int a, int b)
+        {
+            double sqrt2 = Math.Sqrt(2);
+            int firstSide = (int)((a / 2) / (sqrt2 / 2));
+            int secoundSide = (int)((b / 2) / (sqrt2 / 2));
+
+            if ((firstSide % 2 == 0 && secoundSide % 2 == 0) || (firstSide % 2 == 1 && secoundSide % 2 == 1))
+            {
+                return firstSide * secoundSide + (firstSide + 1) * (secoundSide + 1);
+            }
+
+            return (firstSide + 1) * (secoundSide) + firstSide * (secoundSide + 1);
+        }
+    }
+}
