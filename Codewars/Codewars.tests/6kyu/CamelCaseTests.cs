afae24e01c18af49d1a92f64831be3a7baa1f231
Jakub-Bednarski <jakub.piotr.bednarski@gmail.com>
2021-02-13 00:32:44 +0100

    https://www.codewars.com/kata/517abf86da9663f1d2000003/train/csharp

---
+++ Codewars/Codewars.tests/6kyu/CamelCaseTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Codewars._6kyu;
+
+namespace Codewars.tests._6kyu
+{
+    [TestFixture]
+    class CamelCaseTests
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual("theStealthWarrior", CamelCase.ToCamelCase("the_stealth_warrior"), "CamelCase.ToCamelCase('the_stealth_warrior') did not return correct value");
+            Assert.AreEqual("TheStealthWarrior", CamelCase.ToCamelCase("The-Stealth-Warrior"), "CamelCase.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+        }
+    }
+}
---
+++ Codewars/Codewars/6kyu/CamelCase.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Codewars._6kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/517abf86da9663f1d2000003/train/csharp
+    /// </summary>
+    public class CamelCase
+    {
+        public static string ToCamelCase(string str)
+        {
+            if (str == "" || str == null) return str;
+            var strWithSpaces = Regex.Replace(str, "(-)|(_)", " ");
+            var strList = Regex.Split(strWithSpaces, " ");
+
+            if (strList.Length > 0)
+            {
+                for (int i = 1; i < strList.Length; i++)
+                {
+                    strList[i] = strList[i][0].ToString().ToUpper() + strList[i].Substring(1, strList[i].Length - 1);
+                }
+            }
+
+            return string.Join("", strList);
+        }
+    }
+}
