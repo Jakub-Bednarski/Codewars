8c5e5294ee7c1e17e2bcd9fa2b03e032058ccc7a
Jakub-Bednarski <jakub.piotr.bednarski@gmail.com>
2021-02-12 22:42:27 +0100

    https://www.codewars.com/kata/56747fd5cb988479af000028/train/csharp

---
+++ Codewars/Codewars.tests/7kyu/MiddleCharacterTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Codewars._7kyu;
+
+namespace Codewars.tests._7kyu
+{
+    [TestFixture]
+    class MiddleCharacterTests
+    {
+        [Test]
+        public void GenericTests()
+        {
+            Assert.AreEqual("es", MiddleCharacter.GetMiddle("test"));
+            Assert.AreEqual("t", MiddleCharacter.GetMiddle("testing"));
+            Assert.AreEqual("dd", MiddleCharacter.GetMiddle("middle"));
+            Assert.AreEqual("A", MiddleCharacter.GetMiddle("A"));
+        }
+    }
+}
---
+++ Codewars/Codewars/7kyu/MiddleCharacter.cs
@@ -0,0 +1,14 @@
+﻿namespace Codewars._7kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/56747fd5cb988479af000028/train/csharp
+    /// </summary>
+    public class MiddleCharacter
+    {
+        public static string GetMiddle(string s)
+        {
+            if (s.Length % 2 == 0) return s.Substring(s.Length / 2 - 1, 2);
+            return s[s.Length / 2].ToString();
+        }
+    }
+}
